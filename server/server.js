1520642112fda47f9175fe93261213299519c64a
jamesbaber1 <31859220+jamesbaber1@users.noreply.github.com>
2018-09-09 18:50:23 -0500

    Added GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -10,6 +11,8 @@ var app = express();
 //Allows users to send json to express
 app.use(bodyParser.json());
 
+
+//POST /todos
 app.post("/todos",(req,res) => {
     var todo = new Todo({
         text: req.body.text
@@ -22,6 +25,8 @@ app.post("/todos",(req,res) => {
     })
 });
 
+
+//GET /todos
 app.get('/todos', (req, res)=>{
     Todo.find().then((todos)=>{
         res.send({todos});
@@ -29,6 +34,40 @@ app.get('/todos', (req, res)=>{
         res.status(400).send(e);
     })
 })
+
+//GET /todos/123456
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+
+    //return Error  if ID isnt valid
+    if(!ObjectID.isValid(id))
+    {
+        res.status(404).send();
+    }
+    else
+    {
+        Todo.findById(id).then((todo)=>{
+            
+            //returns error if ther  is no todo
+            if(!todo)
+            {
+                res.status(404).send();
+            }
+            else
+            {
+                res.send({todo});
+            }
+        }).catch((e)=>{
+            res.status(400).send();
+        })
+        //success
+            //if todo - send it back 404 with empty body
+        //error
+            //400 - and send empty body back
+
+    }
+});
+
 
 app.listen(3000, ()=> {
     console.log('Started on port 3000');
